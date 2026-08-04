df3fc585b58df3263f2cdc502ab9afb60e155df2
Prachi Dubey <prachidubey050@gmail.com>
2026-08-04 21:43:35 +0530

    Create Find_Missing_elements.java

---
+++ src/leetcode/Find_Missing_elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            int current = nums[i];
+            int next = nums[i + 1];
+
+            for (int j = current + 1; j < next; j++) {
+                ans.add(j);
+            }
+        }
+        return ans;
+    }
+}
